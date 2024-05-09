45e5405d3e5535fcfdab15bce023f7005c497f86
Kexin-Wei-X201 <wkx611@outlook.com>
2024-05-09 22:37:52 +0800

    [add] spdlog as header -- slow!

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Log Reader
 a qt based log reader to read spd log more efficiently
+
+## Dependencies
+- Qt6.2
+- CMake (3.22)
+- spdlog (1.9.2)
+- boost (1.77.0)
+
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <QDebug>
+#include "spdlog/spdlog.h"
 
 int main()
 {
     qDebug() << "Hello, World!";
+
+    spdlog::info("Hello, World!");
+    spdlog::error("Hello, World!");
     return 0;
 }
